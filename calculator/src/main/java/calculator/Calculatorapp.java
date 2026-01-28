5cf2560431d6a074d72772d3c9e151613e7bd6e9
Uyisam <uyisamaboobakkar@gmail.com>
2026-01-28 12:00:58 +0530

    .

--- calculator/src/main/java/calculator/Calculatorapp.java
+++ calculator/src/main/java/calculator/Calculatorapp.java
@@ -1,22 +1,14 @@
 import java.util.Scanner;
-
 public class SimpleCalculator {
     public static void main(String[] args) {
-        // Create scanner object to read user input
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("Enter first number:");
-        double num1 = input.nextDouble();
-        
+                System.out.println("Enter first number:");
+        double num1 = input.nextDouble();       
         System.out.println("Enter second number:");
         double num2 = input.nextDouble();
-        
-        System.out.println("Choose an operation (+, -, *, /):");
-        char operator = input.next().charAt(0);
-        
+                System.out.println("Choose an operation (+, -, *, /):");
+        char operator = input.next().charAt(0);        
         double result;
-
-        // Perform operation based on operator
         switch (operator) {
             case '+':
                 result = num1 + num2;
@@ -39,8 +31,7 @@ public class SimpleCalculator {
                 System.out.println("Invalid operator!");
                 return;
         }
-
         System.out.println("The result is: " + result);
-        input.close(); // Close the scanner to release resources
+        input.close();
     }
 }
